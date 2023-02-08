b23dd53dcd95d15f3aee1a60488260f0d2b1bda1
noppakornn <noppakorn.5142@gmail.com>
2023-02-08 11:29:28 +0700

    update

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send([{"id":1,"title":"Book 1","author":"Author 1"},
-  {"id":2,"title":"Book 2","author":"Author 2"},
-  {"id":3,"title":"Book 3","author":"Author 3"}]);
+  res.send("Hello World! Hello");
 });
 
 app.listen(port, () => {
